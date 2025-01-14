8a880926ddf2dee13e431d3b9a5a1e2bad22b17b
kemboi22 <kemboielvis22@gmail.com>
2025-01-14 13:18:56 +0300

    refactor: replaced all kickstart names with kobotoolbox

--- build-images.sh
+++ build-images.sh
@@ -18,17 +18,17 @@ reponame="kobotoolbox"
 # Create a new empty container image
 container=$(buildah from scratch)
 
-# Reuse existing nodebuilder-kickstart container, to speed up builds
-if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-kickstart; then
+# Reuse existing nodebuilder-kobotoolbox container, to speed up builds
+if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-kobotoolbox; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-kickstart -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-kobotoolbox -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
 fi
 
 echo "Build static UI files with node..."
 buildah run \
     --workingdir=/usr/src/ui \
     --env="NODE_OPTIONS=--openssl-legacy-provider" \
-    nodebuilder-kickstart \
+    nodebuilder-kobotoolbox \
     sh -c "yarn install && yarn build"
 
 # Add imageroot directory to the container image
